c0d75aedee188acc794eec0a05d7508bc27a8000
Gabriel Silva Andrade <34127765+Gabrielsilvaa@users.noreply.github.com>
2021-03-25 00:52:35 -0300

    Add files via upload

---
+++ spring-with-rabbitmq-producer /src/test/java/com/gabriel/springwithrabbitmqproducer/SpringWithRabbitmqProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.springwithrabbitmqproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithRabbitmqProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
